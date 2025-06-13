747faefb47e95070f199980570391bed85a185cf
MurayamaNachi <a5824108@aoyama.jp>
2025-06-13 17:09:09 +0900

    student2

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function onUpdateIsDone(task) {
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
     task.isDone = !task.isDone;
-    
+
     return task;
 }
 
@@ -67,5 +67,6 @@ function onTaskTitleClicked(task) {
 
     task.isDone = !task.isDone;
 
+    
     return task;
 }
